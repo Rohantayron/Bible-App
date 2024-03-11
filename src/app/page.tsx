6753cde3d5a2858b75eebdba93df111ef9f1d950
Rohantayron <bobonica111@gmail.com>
2024-03-11 18:44:37 +0200

    committry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex a min-h-screen flex-col ">
-      <Navbar />
-
       <Hero />
     </main>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import Link from "next/link";
 
 function Navbar() {
   return (
-    <div className="flex  justify-items-center px-24 z-10  py-4  items-center ">
+    <div className="flex  justify-items-center px-24 z-10  py-4  items-center gap-2 ">
       <div className="font-bold mr-auto flex-1 text-lg">BibleApp</div>
+      <Link href="/bibleread">Bible</Link>
       <Input placeholder="Search..." className="max-w-[750px]" />
 
       <div className="gap-2 flex flex-1 items-center justify-center">
