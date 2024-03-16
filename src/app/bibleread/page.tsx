ec73bb5c88a5a9ec865c2f65f29f6f438f6ef196
kdiffin <vhiunese@gmail.com>
2024-03-16 19:01:28 +0400

    a

---
+++ src/app/bibleread/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Verse from "../../components/verse";
+function page() {
+  return (
+    <div>
+      <Verse />
+    </div>
+  );
+}
+
+export default page;
---
+++ src/components/Verse.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function verse() {
+  return (
+    <div className=" border-gray-200 border  text-xl rounded-md  max-w-[1000px] p-3">
+      <div className="text-sky-500 text-2xl p-1">John 1:1 (ESV)</div>
+      <div className="p-1">
+        In the beginning was the Word, and the Word was with God, and the Word
+        was God
+      </div>
+    </div>
+  );
+}
+
+export default verse;
