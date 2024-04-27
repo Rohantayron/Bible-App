ecc4b3ee6d7ca02aa52c0c88cedf3802043e36d4
kdiffin <vhiunese@gmail.com>
2024-04-27 20:42:46 +0400

    pushed fixed ver of search

--- src/app/bible/search/page.tsx
+++ src/app/bible/search/page.tsx
@@ -6,15 +6,12 @@ import React from "react";
 
 function Page() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  
 
   return (
     <div className="flex items-center flex-col justify-center">
       <div>
         <h1 className="text-foreground-primary pt-20 pb-12 text-7xl justify-normal  font-semibold">
-          Search Bible for: {searchParams.get("value")}
+          Search Bible for: thing
         </h1>{" "}
         <div className=" flex  rounded-md">
           <div className="p-2 flex flex-col">
